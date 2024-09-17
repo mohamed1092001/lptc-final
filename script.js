411b0233bd80c3194abad1389a44d21c19db62a9
mohamed hossam <101831478+mohamed1092001@users.noreply.github.com>
2024-09-17 16:01:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -25,29 +25,35 @@ $(document).ready(function () {
 
         // حساب حجم الخط بناءً على أصغر بُعد
         var minDimension = Math.min(fittedWidth, fittedHeight);
-        var fontSizeMainTitle = minDimension * 0.07; 
-        var fontSizeMidTitle = minDimension * 0.05; 
-        var fontSizeBannerParag = minDimension * 0.032; 
-        var fontSizeMoltakaParag = minDimension * 0.035 ; 
-        var fontSizeGoalBoxNum = minDimension * 0.02 ; 
-        var fontSizeKorsyParag = minDimension * 0.029; 
-        var fontSizeMarsadParag = minDimension * 0.031; 
-        var fontSizeTrainning1Parag = minDimension * 0.025; 
-        var fontSizeGoalBoxText = minDimension * 0.024 ; 
+        var fontSizeMainTitle = minDimension * 0.07;
+        var fontSizeMidTitle = minDimension * 0.05;
+        var fontSizeBannerParag = minDimension * 0.032;
+        var fontSizeMoltakaParag = minDimension * 0.035;
+        var fontSizeGoalBoxNum = minDimension * 0.02;
+        var fontSizeKorsyParag = minDimension * 0.029;
+        var fontSizeMarsadParag = minDimension * 0.031;
+        var fontSizeTrainning1Parag = minDimension * 0.025;
+        var fontSizeGoalBoxText = minDimension * 0.024;
 
         $(".hero .main-page .banner-box .main-title").css("font-size", fontSizeMainTitle + 'px')
         $(".mid-title").css("font-size", fontSizeMidTitle + 'px')
+        $(".mid-title-en").css("font-size", (fontSizeMidTitle * 3.5  /5) + 'px')
         $(".hero .main-page .banner-box p").css("font-size", fontSizeBannerParag + 'px')
         $(".hero .main-page .banner-box button").css("font-size", fontSizeBannerParag + 'px')
         $(".back-btn").css("font-size", fontSizeBannerParag + 'px')
         $(".branch p").css("font-size", fontSizeBannerParag + 'px')
         $(".branch .qr-box p").css("font-size", fontSizeBannerParag + 'px')
         $(".moltaka p").css("font-size", fontSizeMoltakaParag + 'px')
+        $(".moltaka-en p").css("font-size", (fontSizeMoltakaParag * 4 / 5) + 'px')
         $(".goal-box .num").css("font-size", fontSizeGoalBoxNum + 'px')
         $(".korsy p").css("font-size", fontSizeKorsyParag + 'px')
+        $(".korsy-en p").css("font-size", (fontSizeKorsyParag * 3.5 / 5) + 'px')
         $(".marsad p").css("font-size", fontSizeMarsadParag + 'px')
+        $(".marsad-en p").css("font-size", (fontSizeMarsadParag * 3.5 / 5) + 'px')
         $(".trainning1 p").css("font-size", fontSizeTrainning1Parag + 'px')
+        $(".trainning1-en p").css("font-size", fontSizeTrainning1Parag + 'px')
         $(".goal-box p").css("font-size", fontSizeGoalBoxText + 'px')
+        $(".goals-con-en .goal-box p").css("font-size", (fontSizeGoalBoxText * 4 / 5) + 'px')
     }
 
     // أول مرة لما الصفحة تتحمل
@@ -61,26 +67,34 @@ $(document).ready(function () {
 
     // screens and button
 
-    $(".hero .main-page .banner-box .btn-container .menu-ar-btn").click(function(){
+    // menu
+
+    $(".hero .main-page .banner-box .btn-container .menu-ar-btn").click(function () {
         $(".section").slideUp(800);
-        $(".menu").slideDown(800);
+        $(".menu-ar").slideDown(800);
     })
 
-    $(".menu .back-btn").click(function(){
+    $(".hero .main-page .banner-box .btn-container .menu-en-btn").click(function () {
+        $(".section").slideUp(800);
+        $(".menu-en").slideDown(800);
+    })
+    // back btn
+    $(".menu .back-btn").click(function () {
         $(".section").slideUp(800);
         $(".main-page").slideDown(800);
     })
-    $(".branch .back-btn").click(function(){
+
+    $(".branch .back-btn").click(function () {
         $(".section").slideUp(800);
         $(".menu").slideDown(800);
         $(".branch video").trigger("pause")
         $(".branch video").prop('currentTime', 0);
     })
 
-    $(".menu-btn").click(function(){
-        let pageTarget=$(this).attr("target")
+    $(".menu-btn").click(function () {
+        let pageTarget = $(this).attr("target")
         $(".section").slideUp(800);
-        $("."+pageTarget).slideDown(800);
+        $("." + pageTarget).slideDown(800);
     })
 
 });
